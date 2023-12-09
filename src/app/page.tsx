0fba63f9026d7e8b5ecf88f3ae746faf3106d7bd
cjsidler <38596474+cjsidler@users.noreply.github.com>
2023-12-08 20:05:33 -0800

    hello world backend route, skeleton home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,23 @@
+"use client";
+
+import { FormEvent } from "react";
+
 export default function Home() {
+    function handleSubmit(event: FormEvent) {
+        event.preventDefault();
+        console.log("Generating color palette!");
+        // TODO - Hit backend route to generate color palette
+        // TODO - Save color array in state variable to trigger re-render with colors displayed to the screen
+    }
+
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
             <div>
-                <h1>Hello, world!</h1>
+                <h1>AI Color Palette Generator</h1>
+                <form action="" onSubmit={handleSubmit}>
+                    <input type="text" />
+                    <button type="submit">Generate</button>
+                </form>
             </div>
         </main>
     );
