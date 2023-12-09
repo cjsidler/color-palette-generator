2ff08844ecd3360929f945885f5a36a478997119
cjsidler <38596474+cjsidler@users.noreply.github.com>
2023-12-09 12:13:29 -0800

    styles, font, layout, click events

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Space_Mono } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const spaceMono = Space_Mono({ subsets: ["latin"], weight: "400" });
 
 export const metadata: Metadata = {
     title: "AI Color Palette Generator",
@@ -12,7 +12,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="en">
-            <body className={inter.className}>{children}</body>
+            <body className={spaceMono.className}>{children}</body>
         </html>
     );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,42 +7,61 @@ interface PalettePayload {
 }
 
 export default function Home() {
-    const [palette, setPalette] = useState<string[]>([]);
+    const [palette, setPalette] = useState<string[]>([
+        "#F2A540",
+        "#F4813E",
+        "#E3604F",
+        "#A4566D",
+        "#5A355F",
+    ]);
     const [isLoading, setIsLoading] = useState(false);
 
     async function handleSubmit(event: FormEvent) {
         event.preventDefault();
-        console.log("Generating color palette!");
 
+        console.log("Color palette: Generating...");
         setIsLoading(true);
 
         const res = await fetch("/api/palette", { method: "POST" });
         const data: PalettePayload = await res.json();
         setPalette(data.palette);
 
+        console.log("Color palette: Done!");
         setIsLoading(false);
     }
 
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            <div>
-                <h1>AI Color Palette Generator</h1>
-                <form action="" onSubmit={handleSubmit}>
-                    <input
-                        type="text"
-                        placeholder={`Enter a prompt (e.g., a california sunset at the beach)`}
-                    />
-                    <button type="submit" disabled={isLoading}>
-                        Generate
-                    </button>
-                </form>
-            </div>
-            <div>
-                <ul>
+        <main>
+            <div className="absolute h-screen w-screen">
+                <div className="flex h-screen w-screen">
                     {palette.map((color, i) => (
-                        <li key={i}>{color}</li>
+                        <div
+                            key={i}
+                            className="flex grow items-end justify-center pb-6"
+                            style={{ backgroundColor: color }}
+                            onClick={() => console.log(`Clicked ${color}`)}
+                        >
+                            <h3>{color}</h3>
+                        </div>
                     ))}
-                </ul>
+                </div>
+            </div>
+            <div className="absolute flex h-screen w-screen items-center justify-center pointer-events-none">
+                <span className="pointer-events-auto">
+                    <h1>AI Color Palette Generator</h1>
+                    <form action="" onSubmit={handleSubmit}>
+                        <div className="flex justify-between px-4">
+                            <input
+                                type="text"
+                                className="flex-grow mr-4"
+                                placeholder={`Enter a prompt (e.g., a california sunset at the beach)`}
+                            />
+                            <button type="submit" disabled={isLoading}>
+                                Generate
+                            </button>
+                        </div>
+                    </form>
+                </span>
             </div>
         </main>
     );
