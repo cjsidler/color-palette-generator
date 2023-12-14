2ff3ed859b29d3d31d9a4254e4ed3d8f5339b426
cjsidler <38596474+cjsidler@users.noreply.github.com>
2023-12-14 09:48:18 -0800

    switch to gpt-3.5-turbo

--- src/app/api/palette/route.ts
+++ src/app/api/palette/route.ts
@@ -1,4 +1,5 @@
 import OpenAI from "openai";
+import { APIPromise } from "openai/core.mjs";
 
 const openai = new OpenAI({ apiKey: process.env.OPEN_AI_KEY });
 
@@ -7,31 +8,38 @@ export async function POST(request: Request) {
     const userPrompt = body.prompt;
     if (!userPrompt) return new Response("Error: Must include prompt", { status: 400 });
 
-    const gptPrompt = `You are a color palette generating assistant that responds to text prompts with color palettes.
-    Color palettes should consist of 5 colors.
-    Color palettes should fit the theme, mood, or instructions in the text prompt.
+    const params: OpenAI.Chat.ChatCompletionCreateParams = {
+        model: "gpt-3.5-turbo",
+        messages: [
+            {
+                role: "system",
+                content:
+                    "You are a color palette generating assistant that responds to text prompts with color palettes. Color palettes should consist of 5 colors. Color palettes should fit the theme, mood, or instructions in the text prompt. Desired format: JSON array of hexadecimal color codes",
+            },
+            { role: "user", content: "Text prompt: a california beach sunset" },
+            {
+                role: "assistant",
+                content: `["#F2A540", "#F4813E", "#E3604F", "#A4566D", "#5A355F"]`,
+            },
+            { role: "user", content: "Text prompt: ice cream parlor flavors" },
+            {
+                role: "assistant",
+                content: `["#6B3E26", "#FFC5D9", "#C2F2D0", "#FDF5C9", "FFCB85"]`,
+            },
+            { role: "user", content: "Text prompt: vintage christmas cards" },
+            {
+                role: "assistant",
+                content: `["#A32C28", "#1C090B", "#384030", "#7B8055", "#BCA875"]`,
+            },
+            { role: "user", content: `Text prompt: ${userPrompt}` },
+        ],
+    };
 
-    Desired format: JSON array of hexadecimal color codes
+    const openAIRes: OpenAI.Chat.ChatCompletion = await openai.chat.completions.create(params);
+    const content = openAIRes.choices[0].message.content;
+    if (!content) return new Response("An error occurred.", { status: 500 });
 
-    Text prompt: a california beach sunset
-    Response: ["#F2A540", "#F4813E", "#E3604F", "#A4566D", "#5A355F"]
-
-    Text prompt: ice cream parlor flavors
-    Response: ["#6B3E26", "#FFC5D9", "#C2F2D0", "#FDF5C9", "FFCB85"]
-
-    Text prompt: vintage christmas cards
-    Response: ["#A32C28", "#1C090B", "#384030", "#7B8055", "#BCA875"]
-
-    Text prompt: ${userPrompt}
-    Response: `;
-
-    const openAIRes: OpenAI.Completion = await openai.completions.create({
-        model: "text-davinci-003",
-        prompt: gptPrompt,
-        max_tokens: 100,
-    });
-
-    const palette = JSON.parse(openAIRes.choices[0].text);
+    const palette = JSON.parse(content);
 
     return Response.json({ palette });
 }
